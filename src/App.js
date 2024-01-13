7debe78734ab6a57110c46288d34a914a976c592
Shubhkiller <shubhamtripathi033@gmail.com>
2024-01-13 11:57:19 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  <header className="header">
+    <h1>css flexbox photo gallery</h1>
+  </header>
+  <div className="gallery">
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg" alt="sleeping" />
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/2.jpg" alt="dumbb" />
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/3.jpg" alt="upside" />
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/4.jpg" alt="dangerous" />
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/5.jpg" alt="staree" />
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/6.jpg" alt="twins" />
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/7.jpg" alt="cover" />
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/8.jpg" alt="curious" />
+    <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/9.jpg" alt="laying" />
+  </div>
+</div>
+  );
+}
+
+export default App;
